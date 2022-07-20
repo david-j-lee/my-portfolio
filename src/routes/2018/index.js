0e79567a154ac20db36934ce073f06d540d8b448
David Lee <devdavidlee@gmail.com>
2022-07-19 21:47:54 -0700

    Fix styling issues from archived sites - updated some styling

--- src/routes/2018/index.js
+++ src/routes/2018/index.js
@@ -4,13 +4,14 @@ import App from "./containers/App/App";
 // eslint-disable-next-line
 import particlesJS from "particles.js/particles";
 
-import "bootstrap/dist/css/bootstrap.min.css";
+// import "bootstrap/dist/css/bootstrap.min.css";
 import "animate.css";
 
-const Year2018 = () => (
-  <div className="year-2018">
-    <App />
-  </div>
-);
+const Year2018 = () => {
+  document.body.classList.remove(...document.body.classList);
+  document.body.classList.add("year-2018");
+
+  return <App />;
+};
 
 export default Year2018;
